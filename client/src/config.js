53e5a234470aa689c48aa88ff011f68390863fd7
jjones254 <86092532+jjones254@users.noreply.github.com>
2022-04-20 18:11:56 -0500

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ let URL;
 if (process.env.NODE_ENV === "development") {
   URL = "http://localhost:5000";
 } else if (process.env.NODE_ENV === "production") {
-  URL = "";
+  URL = "https://school-database-p.herokuapp.com/";
 }
 
 export default URL;
